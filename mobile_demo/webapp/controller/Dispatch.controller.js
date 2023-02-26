27a7415d7a165bb9f6a96f0d6cbff4e6614ff37a
quxiaowei <quxiaowei.work@gmail.com>
2023-02-26 22:55:29 +0800

    add: biz trip

--- mobile_demo/webapp/controller/Dispatch.controller.js
+++ mobile_demo/webapp/controller/Dispatch.controller.js
@@ -4,6 +4,27 @@ sap.ui.define([
     
     "use strict";
     return Controller.extend("mobiledemo.controller.Dispatch", {
+        onInit: function(){
+            var model = new sap.ui.model.json.JSONModel();
+            this.rows = [{ }];
+            model.setData({modelData: this.rows});
+            var oTable = this.byId("dispatchDetailTable")
+            oTable.setModel(model).bindRows("/modelData");
+        },
 
+        onAddTripTable: function(){
+            let oTable = this.byId("dispatchDetailTable");
+            this.rows.push({});
+            oTable.getModel().refresh();
+        },
+
+        onDeleteTripTable: function(oEvent){
+            let oTable = this.byId("dispatchDetailTable");
+            let row = oEvent.getParameters().row;
+            let index = row.getIndex();
+
+            this.rows.splice(index, 1);
+            oTable.getModel().refresh();
+        }
     });
 });
