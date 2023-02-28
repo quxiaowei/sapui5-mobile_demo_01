dc86ce3ec930277621c196fd2b761abb0ba36708
quxiaowei <quxiaowei@github.com>
2023-02-28 18:19:29 +0800

    dev

--- mobile_demo/webapp/controller/Dispatch.controller.js
+++ mobile_demo/webapp/controller/Dispatch.controller.js
@@ -8,24 +8,30 @@ sap.ui.define([
             var deviceModel = this.getModel("device");
             var isPhone = deviceModel.getProperty("/system/phone"); //true false
 
-            var model = new sap.ui.model.json.JSONModel();
             this.rows = [{ }];
-            model.setData({modelData: this.rows});
+            this.model = new sap.ui.model.json.JSONModel();
+            this.model.setData({modelData: this.rows});
+
+            // alert("onInit");
 
             this.fragmentName = isPhone? "mobiledemo.fragment.SubDispatchTripView" : "mobiledemo.fragment.SubDispatchTableView";
 
             this.loadFragment({name: this.fragmentName}).then(function(myFragment){
+                
+                // alert("onLoadFragment");
+
                 this.fragment = myFragment;
                 var oView = this.byId("subArea");
                 oView.addAggregation("items", this.fragment);
-                if(!isPhone){
-                    var oTable = this.byId("dispatchDetailTable");
-                    if(oTable){
-                        oTable.setModel(model).bindRows("/modelData");
-                    }
-                }
+                // if(!isPhone){
+                //     var oTable = this.byId("dispatchDetailTable");
+                //     if(oTable){
+                //         oTable.setModel(this.model).bindRows({path: "/modelData", templateShareable: false});
+                //     }
+                // }
             }.bind(this));
         },
+
 
         onAddTripTable: function(){
             let oTable = this.byId("dispatchDetailTable");
--- mobile_demo/webapp/controller/Main.controller.js
+++ mobile_demo/webapp/controller/Main.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
         onInit: function(){ 
             var deviceModel = this.getModel("device");
             var isPhone = deviceModel.getProperty("/system/phone"); //true false
-            this.fragmentName = isPhone ? "mobiledemo.fragment.SubVBoxLayout" : "mobiledemo.fragment.SubGridLayout";
+            this.fragmentName = isPhone ? "mobiledemo.fragment.SubMainVBoxLayout" : "mobiledemo.fragment.SubMainGridLayout";
             this.loadFragment({name:this.fragmentName}).then(function(myFragment){
                 this.fragment = myFragment;
                 var oView = this.byId("subArea");
